048b5dec08824bafc5d4f4dbab5225ca50e2b614
Federico Juretich <fedejuret@gmail.com>
2022-03-27 23:55:47 -0300

    Adding .env

--- modules/database/connection.go
+++ modules/database/connection.go
@@ -4,11 +4,23 @@ import (
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 )
 
 func GetConnection() *sql.DB {
 
-	db, err := sql.Open("mysql", "root:123456789@/golang_gin_posts?parseTime=true")
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+
+	username := viper.GetString("DB_USERNAME")
+	password := viper.GetString("DB_PASSWORD")
+	host := viper.GetString("DB_HOST")
+	port := viper.GetString("DB_PORT")
+	database := viper.GetString("DB_DATABASE")
+
+	connectionString := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + database + "?parseTime=true"
+
+	db, err := sql.Open("mysql", connectionString)
 
 	if err != nil {
 		panic(err.Error())
